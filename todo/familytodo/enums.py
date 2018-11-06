73187bc3017c08a796cee576002ee0b15dc8288d
Aljaz Glavac <aljaz.glavac@gmail.com>
2018-11-06 21:17:14 +0100

    fresh application

---
+++ todo/familytodo/enums.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+''' Importance enum '''
+class Importance(Enum):
+    HIGH = 'High'
+    MEDIUM = 'Medium'
+    LOW = 'Low'
+
+''' Parent type enum '''
+class ParentType:
+    FATHER = 'Father'
+    MOTHER = 'Mother'
---
+++ todo/familytodo/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+from django import forms
+from django.core.validators import MinValueValidator
+from .enums import Importance
+
+''' Family model '''
+class Family(models.Model):
+    family_name = models.CharField('Family name', max_length=30)
+    password = models.CharField('Password', max_length=30)
+
+    ''' overwrite of default __str__ method, returns name of family '''
+    def __str__(self):
+        return self.family_name
+
+''' Family model forms '''
+class FamilyForm(forms.ModelForm):
+    ''' meta class so password will be masked '''
+    class Meta:
+        model = Family
+        fields = "__all__"
+        widget = {'password': forms.PasswordInput()}
+
+''' Parent model '''
+class Parent(models.Model):
+    parent_name = models.CharField('Parent name', max_length=30)
+    ''' Family can have two parents but parent can have only one family, many-to-one '''
+    parent_family = models.ForeignKey(Family, on_delete=models.CASCADE, verbose_name='Parent family')
+
+    ''' overwrite of default __str__ method, returns name of parent '''
+    def __str__(self):
+        return self.parent_name
+
+''' Child model '''
+class Child(models.Model):
+    child_name = models.CharField('Children name', max_length=30)
+    ''' Family can have many children but child can have only one family, many-to-one '''
+    child_family = models.ForeignKey(Family, on_delete=models.CASCADE, verbose_name='Family of child')
+    
+    ''' overwrite of default __str__ method, returns name of child '''
+    def __str__(self):
+        return self.child_name
+
+''' Task model '''
+class Task(models.Model):
+    ''' task name/description '''
+    task_name = models.CharField('Task', max_length=30)
+    ''' taks importance, enum field(high, medium, low) '''
+    task_importance = models.CharField('Importance', max_length=3, choices=[(tag, tag.value) for tag in Importance])
+    ''' task reward, parent addes reward when completed, can be null/blak '''
+    task_reward = models.CharField('Reward', max_length=30, null=True, blank=True)
+    ''' days to finish taks, 0 means no limit, only pozitive integers '''
+    task_due = models.IntegerField('Due days', default=0, validators=[MinValueValidator(0)])
+    ''' taks belongs to one family and to one child '''
+    task_family = models.OneToOneField(Family, on_delete=models.CASCADE, verbose_name="Task belongs to family", null=True)
+    task_child = models.OneToOneField(Child, on_delete=models.CASCADE, verbose_name="Task belongs to child", null=True)
+
+    ''' overwrite of default __str__ method, returns name of task '''
+    def __str__(self):
+        return self.task_name
