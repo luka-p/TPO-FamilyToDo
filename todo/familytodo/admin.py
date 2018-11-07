3279e3bab9bc2ee1af7ea0e0e0ace11a44e913d5
Aljaz Glavac <aljaz.glavac@gmail.com>
2018-11-07 21:31:46 +0100

    register all the models for better view in admin page

--- todo/familytodo/admin.py
+++ todo/familytodo/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Family, Parent, Child, Task
 
-# Register your models here.
+''' Admin model register '''
+@admin.register(Family)
+class FamilyAdmin(admin.ModelAdmin):
+    ''' columns to display on admin page '''
+    list_display=['family_name', 'password', 'easy_password']
+
+''' Parent model register '''
+@admin.register(Parent)
+class ParentAdmin(admin.ModelAdmin):
+    ''' columns to display on admin page '''
+    list_display=['parent_name', 'parent_family']
+
+''' Child model register '''
+@admin.register(Child)
+class ChildAdmin(admin.ModelAdmin):
+    ''' columns to display on admin page '''
+    list_display=['child_name', 'child_family']
+
+''' Task model register '''
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    ''' columns to display on admin page '''
+    list_display=['task_name', 'task_complete', 'task_importance', 'task_reward', 'task_due', 'task_family', 'task_child'] 
+
