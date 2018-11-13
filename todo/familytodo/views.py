ae40e8f033e87c7d34816fb036aa6ea8c591391f
Aljaz Glavac <aljaz.glavac@gmail.com>
2018-11-13 18:01:46 +0100

    views only with login and registration forms

--- todo/familytodo/views.py
+++ todo/familytodo/views.py
@@ -1,3 +1,46 @@
+from django.views.decorators.http import require_http_methods
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from .forms import (FamilyRegistrationForm,
+                    FamilyLoginForm,
+                    ChildLoginForm,
+                    TaskAddForm)
 
-# Create your views here.
+def index(request):
+    return None
+
+@require_http_methods(["GET", "POST"])
+def register_family(request):
+    if request.method == 'POST':
+        form = FamilyRegistrationForm(request.POST)
+        if form.is_valid():
+
+            return HttpResponseRedirect('')
+    else:
+        form = FamilyRegistrationForm()
+
+    return render(request, 'family_register.html', {'form': form})
+
+@require_http_methods(["GET", "POST"])
+def login_parent(request):
+    if request.method == 'POST':
+        form = FamilyLoginForm(request.POST)
+        if form.is_valid():
+
+            return HttpResponseRedirect('')
+    else:
+        form = FamilyLoginForm()
+
+    return render(request, 'parent_login.html', {'form': form})
+
+@require_http_methods(["GET", "POST"])
+def login_child(request):
+    if request.method == 'POST':
+        form = ChildLoginForm(request.POST)
+        if form.is_valid():
+            
+            return HttpResponseRedirect('')
+    else:
+        form = ChildLoginForm()
+
+    return render(request, 'child_login.html', {'form': form})
