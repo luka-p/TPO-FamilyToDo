e9b4fe12dfe41553f57cbe03e94b2cbbfda42e6d
aljazglavac <aljaz.glavac97@gmail.com>
2018-11-21 10:58:56 +0100

    account type tuple coment

--- familytodo/models.py
+++ familytodo/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+''' tuple of account types '''
 ACTYPE = (
     ('Free', 'Free'),
     ('Paid', 'Paid'))
