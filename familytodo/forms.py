ab2864c236a63966f5802a0f495708886774d2f9
Andraz Raspor <andraz.raspor@gmail.com>
2018-12-17 22:33:04 +0100

    forgot to add everything

--- familytodo/forms.py
+++ familytodo/forms.py
@@ -1,4 +1,10 @@
 from django import forms
+
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, Layout, Field
+from crispy_forms.bootstrap import (
+    PrependedText, PrependedAppendedText, FormActions)
+
 ''' import child from models because this is dynamic
 so it changes or can be changed every time that TaskAddForm is called
 and because of that we need to refresh choices every time we call this form '''
@@ -22,7 +28,7 @@ class FamilyRegistrationForm(forms.Form):
     family_password = forms.CharField(label="Full password", max_length=30, widget=forms.PasswordInput)
     family_easy_password = forms.IntegerField(label="Easy password", min_value=0000, max_value=9999, widget=forms.PasswordInput, error_messages=err_msg)
     account_type = forms.ChoiceField(label="Account type", choices=ACTYPE, widget=forms.RadioSelect)
-    
+
 
 class FreeParentForm(forms.Form):
     ''' Free users can add only one parent '''
@@ -39,6 +45,10 @@ class FamilyLoginForm(forms.Form):
     family_parent = forms.CharField(label="Parent name", max_length=30, strip=True)
     family_password = forms.CharField(label="Family password", max_length=30, widget=forms.PasswordInput)
 
+    helper = FormHelper()
+    helper.form_method = 'POST'
+    helper.add_input(Submit('Login', 'login', css_class='btn-primary'))
+
 ''' Child login form '''
 class ChildLoginForm(forms.Form):
     family_username = forms.CharField(label="Family username", max_length=30, strip=True)
@@ -51,7 +61,7 @@ class ChildAddForm(forms.Form):
 ''' Task adding form '''
 class TaskAddForm(forms.Form):
     task_name = forms.CharField(label="Task name", max_length=30, strip=True)
-    task_importance = forms.ChoiceField(label="Task importance", choices=IMPORTANCE, initial='MEDIUM') 
+    task_importance = forms.ChoiceField(label="Task importance", choices=IMPORTANCE, initial='MEDIUM')
     task_reward = forms.CharField(label="Task reward", max_length=30, strip=True, required=False)
     task_due = forms.IntegerField(label="Taks due days", min_value=0, error_messages=err_msg)
     task_child = forms.ChoiceField(label="Child", choices=[(c.child_name, c.child_name) for c in Child.objects.all()])
